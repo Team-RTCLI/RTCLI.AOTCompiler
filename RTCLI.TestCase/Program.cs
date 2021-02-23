e2b5c7f7b890de55b6da3536022a05ac9741b85c
SaeruHikari <3045349520@qq.com>
2021-02-23 20:20:29 +0800

    target 2.1 => net5.0

--- RTCLI.TestCase/Program.cs
+++ RTCLI.TestCase/Program.cs
@@ -4,12 +4,30 @@ using System.Text;
 
 namespace TestCase
 {
+    public partial struct Value
+    {
+        int v;
+    }
+
+    public partial struct Value
+    {
+        int vv;
+        public int Test(Value o)
+        {
+            return o.vv;
+        }
+    }
+
     public class Program
     {
         public static int Main(string[] args)
         {
             TestCase.TestSystem.Test();
+            
+            Value v = new Value();
+            var t = v.Test(v);
 
+            Console.WriteLine(v.ToString());
             return 0;
         }
     }
